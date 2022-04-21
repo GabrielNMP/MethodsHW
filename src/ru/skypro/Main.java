e7f88123a4ae8ed1d35dc7b6b13ef88829f84d1b
Gabriel_n3 <p705865@gmail.com>
2022-04-21 13:43:19 +0300

    Answers branch

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -2,7 +2,111 @@ package ru.skypro;
 
 public class Main {
 
+
     public static void main(String[] args) {
-	// write your code here
+// Task 1
+        namingTitle("1");
+        chekingLeapYear(2022);
+
+// Task 2
+        namingTitle("2");
+        installingApplications(0, 2010);
+
+// Task 3
+        namingTitle("3");
+        int daysDelivery = calculateDeliveryDistance(95);
+        System.out.println("На доставку понадобится - " + daysDelivery + " дня." );
+
+// Task 4
+
+        namingTitle("4");
+        serchingDuble("aabccddefgghiijjkk");
+
+// Task 5
+
+        namingTitle("5");
+        int[] arr = {3, 2, 1, 6, 5};
+        reverceArray(arr);
+
+
+
+    }
+
+    public static void namingTitle(String name) {
+        System.out.println();
+        System.out.println("___________________");
+        System.out.println("Задание " + name + ". Ответы.");
+
+    }
+
+    public static void chekingLeapYear(int year) {
+        if (year % 4 == 0 && year / 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " - это високосный год. ");
+        } else {
+            System.out.println(year + " - это не високосный год. ");
+        }
+    }
+
+    public static void installingApplications (int clientOs, int clientDeviceYear) {
+        if (clientOs == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Вы можете установить версию для iOS по ссылке: ");
+        }
+        if (clientOs == 0 && clientDeviceYear < 2015) {
+            System.out.println("Вы можете установить облегчённую версию для iOS по ссылке: ");
+        }
+        if (clientOs != 0 && clientDeviceYear >= 2015) {
+            System.out.println("Вы можете установить версию для Android по ссылке:");
+        }
+        if (clientOs != 0 && clientDeviceYear < 2015) {
+            System.out.println("Вы можете установить облегчённую версию для Android по ссылке:");
+        }
+
+    }
+
+    public static int calculateDeliveryDistance ( int deliveryDistance) {
+        int days = 0;
+        //int daysDelivery = 0;
+        if (deliveryDistance < 20) {
+            days = days + 1;
+            //System.out.println("На доставку потребуется " + daysDelivery + " день.");
+        } else {
+            if (deliveryDistance < 60) {
+                days = days + 2;
+                //System.out.println("На доставку потребуется " + daysDelivery + " дня.");
+
+            } else {
+                days = days + 3;
+                //System.out.println("На доставку потребуется " + daysDelivery + " дня."); }
+            }
+        }
+        return days;
+    }
+
+    public static void serchingDuble(String duble) {
+        System.out.println("Проверяемая строка - " + duble);
+        for (int i = 0; i < duble.charAt(i)-1; i++) {
+            if (duble.charAt(i) == duble.charAt(i+1)) {
+                System.out.println("В строке есть дубль, первый задублированный символ - " + duble.charAt(i));
+                break;
+            } else {
+                System.out.println("В строке нет дублей");
+                break;
+            }
+        }
+
+    }
+
+    public static void reverceArray (int[] arr) {
+        System.out.println("Заданный массив: ");
+        for (int i = 0 ; i <= arr.length -1; i++ ) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        System.out.println("Перевернутый массив: ");
+        for (int i = arr.length - 1; i >= 0; i-- ) {
+            System.out.print(arr[i] + " ");
+        }
+
     }
 }
+
